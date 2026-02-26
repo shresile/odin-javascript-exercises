69a65f52c0f5d2ef692347885b81b2ab09166d6e
shresile <46599124+shresile@users.noreply.github.com>
2026-02-26 18:16:03 +0800

    Complete exercise 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+    let newStr = str;
+    if (num < 0){
+        return 'ERROR';
 
+    } else if (num === 0){
+        return '';
+    } else {
+    
+    for (i=0;i<num-1;i++){
+        
+        newStr =newStr + str;
+        
+    }
+    return newStr;}
 };
 
 // Do not edit below this line
