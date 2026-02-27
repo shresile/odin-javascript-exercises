563aff96b5e5cd5f60ace2a2ebf9e0fdb2fc1736
shresile <46599124+shresile@users.noreply.github.com>
2026-02-27 16:57:09 +0800

    Complete exercise 08
    I should learn to think in a different angle like the solution, my method
    is a bit complicated, and less smart.
    Perhaps getting more familiar with array's methods will help.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
 const removeFromArray = function() {
+    //loop items in arr to check if any item is the same as argument "a"
+    //if it's true,use splice(index,1) to delete it
+    //for (i = 0; i < arr.length; i++){
+       // if (arr[i] === a){
+            //arr.splice(i,1);
+            //return arr;
+        //}
+   // }
+
+    //for multiple argument, like arr,a,b,c..,convert arguments to an array
+    let args = Array.from(arguments);
+    //loop start from args[1]
+    for (j = 1; j < args.length; j++){
+        for (i = 0; i < args[0].length; i++){
+                if (args[0][i] === args[j]){
+                    args[0].splice(i,1);
+                    i--;
+                    
+                }
+            }
+    }
+    return args[0];
 };
 
 // Do not edit below this line
