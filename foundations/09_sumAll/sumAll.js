509b71abd22e13bfa884caa706eb59bf59bf9e01
shresile <46599124+shresile@users.noreply.github.com>
2026-02-27 22:25:30 +0800

    Complete exercise 09
    when the value is a non-number parameter, Number.isInteger(value)
    return false, so there is no need to use typeof value !=='number' after we
    use Number.isInteger(value)

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    //if a or b is negative/non-integer/non-number, return error, 
+    // when the value is a non-number parameter, Number.isInteger(value) return false
+    const arr = [];
+    if ((a <= 0 || b<=0) ||(!Number.isInteger(a) || !Number.isInteger(b))){
+        return "ERROR";
+    } else if (a > b) {
+        for (i = 0; i <= a-b; i++){
+            arr.push(b+i);
+        }
+    } else {
+        for (i = 0; i <= b-a; i++) {
+            arr.push(a+i);
+        }
+    }
 
+    return arr.reduce((sum, current) => sum + current, 0);
+    
 };
 
 // Do not edit below this line
