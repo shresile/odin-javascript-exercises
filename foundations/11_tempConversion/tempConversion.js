0dded1065942601528acda6d8e8a05cb8868518e
shresile <46599124+shresile@users.noreply.github.com>
2026-02-28 14:28:53 +0800

    Complete exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const convertedToCelsius = (fahrenheit-32)*5/9;
+  return +convertedToCelsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const convertedToFahrenheit = celsius*9/5+32;
+  return +convertedToFahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
