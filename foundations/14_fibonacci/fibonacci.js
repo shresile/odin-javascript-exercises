f7e1d775196c9d701ff3a0a486ac4ee04f086897
shresile <46599124+shresile@users.noreply.github.com>
2026-03-18 12:18:07 +0800

    Complete exercise 14-16

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if (+input === 0) {
+        return 0;
+    } else if (+input < 0) {
+        return "OOPS";
+    }
+    let index = +input - 1;
+    let fibonacciArr = [1, 1];
+    for (i = 2; i <= index; i++) {
+        fibonacciArr[i] = fibonacciArr[i-2] + fibonacciArr[i-1];
+    }
+    return fibonacciArr[index];
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    
+    arr.sort((a,b) => {
+        return computeAge(b) - computeAge(a);       
 
+    });
+    
+   return arr[0];
+
+    function computeAge (current) {
+        let computedAge;
+        if (current.yearOfDeath == undefined) {
+            computedAge = new Date().getFullYear() - current.yearOfBirth;
+        } else {
+        computedAge = current.yearOfDeath - current.yearOfBirth;
+
+        }
+        return computedAge;
+    }
 };
 
 // Do not edit below this line
